60786734853ae874bad31aa41f807e9ddf8c79e9
Shivam6735 <foodieshivam09@gmail.com>
2025-12-27 10:48:30 +0530

    Add Initial Files

---
+++ BASICS/lecture1.py
@@ -0,0 +1,18 @@
+# n=5
+# for i in range(1,n+1):
+#     print("Shukla")
+
+
+
+
+age = 10
+if age>=80:
+    print("Super Senior")
+elif age >= 60 and age<80:
+    print("Senior")
+elif age >= 40 and age<60:
+    print("Batchmate")
+elif age >= 20 and age<40:
+    print("Junior")
+elif age<20:
+    print("Super Junior")
